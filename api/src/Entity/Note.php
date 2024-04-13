758e433d2f9870d9cbc5915e2b5e4381d033cd8b
Michael Milawski <mm@millsoft.de>
2024-04-13 19:16:04 +0200

    feat: downgrade symfony

--- api/src/Entity/Note.php
+++ api/src/Entity/Note.php
@@ -26,6 +26,9 @@ class Note
     #[ORM\Column(type: Types::TEXT)]
     private ?string $content = null;
 
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
 
     public function getId(): ?Uuid
     {
@@ -52,6 +55,18 @@ class Note
     public function setContent(string $content): static
     {
         $this->content = $content;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
 
         return $this;
     }
